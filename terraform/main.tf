55281d23dda5df11c7e874f6be7379c332bb81b7
Venkat Padmanabhan <venkatakrishnan.padmanabhan@gmail.com>
2025-04-14 21:35:33 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "google_compute_instance" "default" {
-  name         = "my-vm"
+  name         = "venkat-test-terraform"
   machine_type = "n1-standard-1"
   zone         = "us-central1-a"
 
