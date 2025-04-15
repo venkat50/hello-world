6c014415d6f722711a009bd14ec2aee3df549e51
Venkat Padmanabhan <venkatakrishnan.padmanabhan@gmail.com>
2025-04-14 21:33:26 -0400

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+resource "google_compute_instance" "default" {
+  name         = "my-vm"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-minimal-2210-kinetic-amd64-v20230126"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
