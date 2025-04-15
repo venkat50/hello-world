7f83d6d97dd59c7e0cccc4df2fa4932076e108e1
Venkat Padmanabhan <venkatakrishnan.padmanabhan@gmail.com>
2025-04-14 23:18:51 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,6 +15,7 @@ resource "google_compute_instance" "default" {
 
 service_account {
     email  = "980596850008-compute@developer.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
 }
 
   network_interface {
